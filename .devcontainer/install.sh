23a7b95138256989a7f013f09fa8088227eda457
Kim jinwook <yoonlight12@gmail.com>
2024-07-13 01:31:58 +0000

    update devcontainer config
    - pip install에서 install bash file 실행으로 변경

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,4 +1,5 @@
 apt-get update
 
 apt-get install -y wget git unzip
-apt-get install -y libgl1 libglib2.0-0 libx264-dev ffmpeg
+DEBIAN_FRONTEND=noninteractive apt-get install -y libgl1 libglib2.0-0 libx264-dev ffmpeg
+pip install -r requirements.txt
